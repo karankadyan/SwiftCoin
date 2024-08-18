87edea2758953f337430957ccc8aeec1f1a407ae
Karan Kadyan <karankadyan20@gmail.com>
2024-08-18 22:40:49 +0530

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Karan Kadyan on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
